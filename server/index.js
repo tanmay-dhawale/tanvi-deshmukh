dff90fc18d198ba54f71e9e689d2fbcef82c6a31
Tanvi Deshmukh <tanmaydhawale333@gmail.com>
2024-01-17 19:49:48 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -25,6 +25,14 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
 
